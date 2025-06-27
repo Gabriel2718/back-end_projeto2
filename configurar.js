9d14158ac0329cad65445fb6f29d6fd016bd55c4
Gabriel <gcr2@outlook.com.br>
2025-06-27 14:42:37 -0300

    Arquivos de inserção de dados de usuários no MongoDB

---
+++ configurar.js
@@ -0,0 +1,22 @@
+const { connect } = require('./db');
+const users = require('./users.json');
+
+async function configurar(){
+    try{
+        const {db, client } = await connect();
+        const colecao = db.collection('users');
+
+        colecao.drop();
+
+        for(i = 0; i < users.length; i++){
+            await colecao.insertOne({
+                user: users[i]['user'],
+                pass: users[i]['pass']
+            });
+        }
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+module.exports = { configurar };

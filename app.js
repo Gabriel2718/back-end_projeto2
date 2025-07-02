e91c0949d0b02c2c5da725afb057bbea31b0990c
Gabriel <gcr2@outlook.com.br>
2025-07-02 15:38:24 -0300

    Caminhos adicionados

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const cookieParser = require('cookie-parser');
 
 const { configurar } = require('./configurar');
 
+configurar();
+
 const app = express();
 
 app.set(
@@ -20,7 +22,50 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({
     extended: false
 }));
+app.use(cookieParser());
+app.use(
+    session({
+        secret: 'segredo_criptografico',
+        resave: false,
+        saveUninitialized: false,
+        cookie: {maxAge: 60000}
+    })
+);
 
-configurar();
+function checkLogin(req, res, next) {
+    if(req.session.logado){
+        next();
+    } else {
+        res.redirect('/login');
+    }
+}
+
+function checkUserInfo() {
+    
+}
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.post('/login', (req, res) => {
+    const {usuario, senha} = req.body; //os valores "name" dos inputs são enviados com a rquisição post
+
+    if(usuario === 'admin' && senha === '123') {
+        req.session.logado = true;
+        res.redirect('/');
+    } else {
+        res.send('Usuário ou senha inválidos <a href="/login">Tentar de novo</a>');
+    }
+});
+
+app.get('/logout', (req, res) => {
+    req.session.destroy();
+    res.redirect('/login');
+});
+
+app.get('/', checkLogin, (req, res) => {
+    res.render('index');
+});
 
 app.listen(3000);
